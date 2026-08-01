9a53e6241769bf3d408045085c75fef8239e5a2f
jamesrochabrun <jamesrochabrun@gmail.com>
2026-07-31 22:08:35 -0700

    Initial Commit

---
+++ CodingBuddy/CodingBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingBuddyApp.swift
+//  CodingBuddy
+//
+//  Created by James Rochabrun on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct CodingBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
